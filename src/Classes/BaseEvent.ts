d8c5912179f43adeda50604e761bdeb7ca579dc0
Derpinou <lightcompte@gmail.com>
2023-01-05 14:47:02 +0100

    fix: types

--- src/Classes/BaseEvent.ts
+++ src/Classes/BaseEvent.ts
@@ -1,14 +1,16 @@
 
-import { BaseClient } from '../index';
+import { BaseClient, BaseModule } from '../index';
 import { ClientEvents } from 'oceanic.js';
 
 
 export abstract class BaseEvent {
 	protected client: BaseClient;
-	protected constructor(client: BaseClient) {
+	protected module: BaseModule;
+	protected constructor(client: BaseClient, module: BaseModule) {
 		this.client = client;
+		this.module = module;
 	}
 
-	abstract run(args: ClientEvents[keyof ClientEvents]): void
+	abstract run(...args: ClientEvents[keyof ClientEvents]): void
 
 }
